a3e287e656addefad353893a76840838bc8988e6
hoangphiAltair <hoangphi@altair.com>
2024-07-04 11:12:36 +0700

    Add files via upload

---
+++ Execute_Python.py
@@ -0,0 +1,23 @@
+
+#import runpy to run Python file through HyperMesh Python window
+import runpy
+#import tkinter to create an open file dialog
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+
+
+root = tk.Tk()
+root.withdraw()  # hide the root window
+#set initial folder as Desktop
+desktop_folder = os.path.join(os.environ['USERPROFILE'], 'Desktop')
+#select script file
+filename = filedialog.askopenfilename(initialdir=desktop_folder,
+                                    title="Select Python Script",
+                                    filetypes=[("Python Script", "*.py")])
+
+with open(filename, 'r') as f:
+    script = f.read()
+exec(script)
+
